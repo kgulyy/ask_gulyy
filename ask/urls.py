3956cc4d61f8bfe4a22c86315bc9f9acbd2e29c2
Konstantin Gulyy <const.gulyy@gmail.com>
2017-04-22 13:46:10 +0300

    added urls and view stabs

--- ask/urls.py
+++ ask/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import url
 from . import views
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^tag/(?P<tag_name>[A-z0-9]+)/$', views.questions_by_tag, name='questions_by_tag'),
+    url(r'^question/(?P<question_id>[0-9]+)/$', views.question_detail, name='question_detail'),
+    url(r'^hot/$', views.hot, name='hot'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^singup/$', views.singup, name='singup'),
+    url(r'^ask/$', views.ask, name='ask'),
 ]
--- ask/views.py
+++ ask/views.py
@@ -3,3 +3,27 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the ask index.")
+
+
+def hot(request):
+    return HttpResponse("Hello, world. You're at the hot url.")
+
+
+def questions_by_tag(request, tag_name):
+    return HttpResponse("You see questions by {0} tag".format(tag_name))
+
+
+def question_detail(request, question_id):
+    return HttpResponse("It's {0} question".format(question_id))
+
+
+def login(request):
+    return HttpResponse("Hello, world. You're at the login url.")
+
+
+def singup(request):
+    return HttpResponse("Hello, world. You're at the singup url.")
+
+
+def ask(request):
+    return HttpResponse("Hello, world. You're at the ask url.")
