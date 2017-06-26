69f7be3d1e04979c522e9ff4b03c3d7a3e286563
Konstantin Gulyy <const.gulyy@gmail.com>
2017-06-27 01:45:37 +0300

    Added template to base

--- ask/urls.py
+++ ask/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^tag/(?P<tag_name>[A-z0-9]+)/$', views.questions_by_tag, name='questions_by_tag'),
+    url(r'^tag/(?P<tag_name>[A-z0-9\.]+)/$', views.questions_by_tag, name='questions_by_tag'),
     url(r'^question/(?P<question_id>[0-9]+)/$', views.question_detail, name='question_detail'),
     url(r'^hot/$', views.hot, name='hot'),
     url(r'^login/$', views.login, name='login'),
